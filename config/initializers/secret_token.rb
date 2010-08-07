336b9641c9402f946c1e25f1a4f22affd7801d11
Justin Tyler Wiley <justin.wiley@gmail.com>
2010-08-07 10:52:45 -0400

    initial insert

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveytool::Application.config.secret_token = '66e653c2a0aa8d6c36c7246283a2987090d8ec4761b4eaf5368fa5c6dab30d87d081b2deac8de23bb91f83086d77ade77ad7a5cf78d483b93b5963b94e9810b7'
