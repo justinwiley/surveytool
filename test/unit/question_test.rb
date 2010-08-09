aa6480404d4aca5989fd619cbd57e211fb596dad
Justin Tyler Wiley <justin.wiley@gmail.com>
2010-08-09 19:49:59 -0400

    removes test directory and adds to ignore

--- test/unit/question_test.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Question do
-  it "should be valid" do
-    Question.new.should be_valid
-  end
-end
--- test/unit/survey_test.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Survey do
-  it "should be valid" do
-    Survey.new.should be_valid
-  end
-end
